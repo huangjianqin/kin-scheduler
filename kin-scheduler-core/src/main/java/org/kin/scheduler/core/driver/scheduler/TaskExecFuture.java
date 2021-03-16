6c4e926eb0c521cbe58f0f69a3c3727c9660bb68
huangjianqin <18814127639@163.com>
2021-03-16 10:56:51 +0800

    future优化

--- kin-scheduler-core/src/main/java/org/kin/scheduler/core/driver/scheduler/TaskExecFuture.java
+++ kin-scheduler-core/src/main/java/org/kin/scheduler/core/driver/scheduler/TaskExecFuture.java
@@ -6,6 +6,7 @@ import org.kin.framework.utils.SysUtils;
 import org.kin.scheduler.core.executor.transport.TaskSubmitResp;
 import org.kin.scheduler.core.task.domain.TaskStatus;
 
+import javax.annotation.Nonnull;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -19,6 +20,7 @@ import java.util.concurrent.TimeUnit;
  * @author huangjianqin
  * @date 2020-02-12
  */
+@SuppressWarnings("rawtypes")
 public class TaskExecFuture<R extends Serializable> implements Future<R> {
     /** callback执行线程池 */
     public static ExecutionContext CALLBACK_EXECUTORS = ExecutionContext.fix(SysUtils.CPU_NUM, "TaskSubmitFuture-Callback-Thread");
@@ -31,14 +33,26 @@ public class TaskExecFuture<R extends Serializable> implements Future<R> {
     private TaskSubmitResp taskSubmitResp;
     /** 所属TaskScheduler */
     private TaskScheduler taskScheduler;
+
+    /** task id */
+    private volatile String taskId;
+    /** task status */
+    private volatile TaskStatus taskStatus;
     /** task执行结果 */
     private volatile R result;
+    /** task log path */
+    private volatile String logPath;
+    /** task output path */
+    private volatile String outputPath;
+    /** task fail reason */
+    private volatile String reason;
+
     /** future是否完成 */
     private volatile boolean done;
     /** future是否取消 */
     private volatile boolean canneled;
     /** 添加的callbacks */
-    private Collection<TaskExecCallback<R>> callbacks = new CopyOnWriteArrayList<>();
+    private final Collection<TaskExecCallback<R>> callbacks = new CopyOnWriteArrayList<>();
     /** 阻塞等待的的线程数 */
     private short waiters;
 
@@ -79,7 +93,7 @@ public class TaskExecFuture<R extends Serializable> implements Future<R> {
     }
 
     @Override
-    public R get(long timeout, TimeUnit unit) throws InterruptedException {
+    public R get(long timeout, @Nonnull TimeUnit unit) throws InterruptedException {
         if (!isDone()) {
             synchronized (this) {
                 waiters++;
@@ -96,9 +110,17 @@ public class TaskExecFuture<R extends Serializable> implements Future<R> {
     /**
      * task 完成调用
      */
+    @SuppressWarnings("unchecked")
     public void done(String taskId, TaskStatus taskStatus, Serializable result, String logPath, String outputPath, String reason) {
         done = true;
+
+        this.taskId = taskId;
+        this.taskStatus = taskStatus;
         this.result = (R) result;
+        this.logPath = logPath;
+        this.outputPath = outputPath;
+        this.reason = reason;
+
         CALLBACK_EXECUTORS.execute(() -> {
             for (TaskExecCallback<R> callback : callbacks) {
                 callback.execFinish(taskId, taskStatus, this.result, logPath, outputPath, reason);
@@ -115,24 +137,29 @@ public class TaskExecFuture<R extends Serializable> implements Future<R> {
      * task 调度失败
      */
     public void fail(String taskId, TaskStatus taskStatus, String reason) {
-        done = true;
-        CALLBACK_EXECUTORS.execute(() -> {
-            for (TaskExecCallback<R> callback : callbacks) {
-                callback.execFinish(taskId, taskStatus, null, "", "", reason);
-            }
-        });
-        synchronized (this) {
-            if (waiters > 0) {
-                notifyAll();
-            }
-        }
+        done(taskId, taskStatus, null, "", "", reason);
     }
 
     /**
      * 添加callback
      */
     public TaskExecFuture<R> addCallback(TaskExecCallback<R> callback) {
+        if (isDone()) {
+            CALLBACK_EXECUTORS.execute(() -> {
+                callback.execFinish(taskId, taskStatus, this.result, logPath, outputPath, reason);
+            });
+        }
         callbacks.add(callback);
+        return this;
+    }
+
+    /**
+     * 批量添加callback
+     */
+    public TaskExecFuture<R> addCallback(TaskExecCallback<R>... callbacks) {
+        for (TaskExecCallback<R> callback : callbacks) {
+            addCallback(callback);
+        }
         return this;
     }
 
